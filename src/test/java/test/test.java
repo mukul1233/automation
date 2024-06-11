845891b5ef2693c56dc37be901de50d4a5f2c6c5
mukul chauhan <mukulchauhan1233@gmail.com>
2024-06-11 18:26:31 +0530

    test changes

--- src/test/java/test/test.java
+++ src/test/java/test/test.java
@@ -21,54 +21,54 @@ import java.util.Iterator;
 import java.util.List;
 
 public class test extends base {
-    WebElement bookDemoButton;
-    WebElement emailInput;
-    WebElement submitEmail;
-    WebElement demoText;
-    WebElement chatFrame;
-    WebElement emailID;
-
-    @Test
-
-    public void buildingTestMethod() {
-
-        String verifyBrowserStackTitle = driver.getTitle();
-        assertEquals("Most Reliable App & Cross Browser Testing Platform | BrowserStack",verifyBrowserStackTitle);
-    }
-
-    @Test
-    public void bookDemo() throws InterruptedException {
-        bookDemoButton= driver.findElement(By.cssSelector("div.btn-text"));
-        bookDemoButton.click();
-
-        emailInput=driver.findElement(By.xpath("(//input[@id='demo-fc-email'])[1]"));
-        emailInput.sendKeys("abc@xyz.com");
-        submitEmail=driver.findElement(By.xpath("(//input[@id='demo-fc-submit'])[1]"));
-        submitEmail.click();
-        WebDriverWait wait = new WebDriverWait(driver,30);
-        wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(By.cssSelector("iframe#fc_widget")));
-        demoText= driver.findElement(By.cssSelector("div.h-message-text"));
-        Assert.assertTrue(demoText.isDisplayed());
-        driver.switchTo().parentFrame();
-    }
-
-    @Test
-    public void takeScreenShot() throws IOException {
-
-        driver.navigate().to("https://demo.guru99.com/");
-        emailID= driver.findElement(By.xpath("//input[@name='emailid']"));
-        emailID.sendKeys("milei.cjaihan@gmail.com", Keys.ENTER);
-
-    }
-
-    @Test
-    public void checkConnection() throws IOException {
-        HttpURLConnection huc=null;
-        String url="https://google.com";
-        huc =(HttpURLConnection) (new URL(url).openConnection());
-        huc.setRequestMethod("GET");
-        huc.connect();
-        System.out.println(huc.getResponseCode()+"\n"+huc.getResponseMessage());
-    }
+//    WebElement bookDemoButton;
+//    WebElement emailInput;
+//    WebElement submitEmail;
+//    WebElement demoText;
+//    WebElement chatFrame;
+//    WebElement emailID;
+//
+//    @Test
+//
+//    public void buildingTestMethod() {
+//
+//        String verifyBrowserStackTitle = driver.getTitle();
+//        assertEquals("Most Reliable App & Cross Browser Testing Platform | BrowserStack",verifyBrowserStackTitle);
+//    }
+//
+//    @Test
+//    public void bookDemo() throws InterruptedException {
+//        bookDemoButton= driver.findElement(By.cssSelector("div.btn-text"));
+//        bookDemoButton.click();
+//
+//        emailInput=driver.findElement(By.xpath("(//input[@id='demo-fc-email'])[1]"));
+//        emailInput.sendKeys("abc@xyz.com");
+//        submitEmail=driver.findElement(By.xpath("(//input[@id='demo-fc-submit'])[1]"));
+//        submitEmail.click();
+//        WebDriverWait wait = new WebDriverWait(driver,30);
+//        wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(By.cssSelector("iframe#fc_widget")));
+//        demoText= driver.findElement(By.cssSelector("div.h-message-text"));
+//        Assert.assertTrue(demoText.isDisplayed());
+//        driver.switchTo().parentFrame();
+//    }
+//
+//    @Test
+//    public void takeScreenShot() throws IOException {
+//
+//        driver.navigate().to("https://demo.guru99.com/");
+//        emailID= driver.findElement(By.xpath("//input[@name='emailid']"));
+//        emailID.sendKeys("milei.cjaihan@gmail.com", Keys.ENTER);
+//
+//    }
+//
+//    @Test
+//    public void checkConnection() throws IOException {
+//        HttpURLConnection huc=null;
+//        String url="https://google.com";
+//        huc =(HttpURLConnection) (new URL(url).openConnection());
+//        huc.setRequestMethod("GET");
+//        huc.connect();
+//        System.out.println(huc.getResponseCode()+"\n"+huc.getResponseMessage());
+//    }
     }
 
